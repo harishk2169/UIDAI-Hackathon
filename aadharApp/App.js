770762833a3714c8c8c8e8db82f23f5bfc3b2f96
gourav2001k <mailatgourav2001@gmail.com>
2021-10-28 11:39:43 +0530

    Repository Setup

---
+++ aadharApp/App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+// import AppNavigator from './navigation/AppNavigator';
+
+const App = () => {
+  return <View></View>;
+};
+
+export default App;
