8e4403183c55cf10fdb456032fe73ea9e596ce7d
gourav2001k <mailatgourav2001@gmail.com>
2021-10-31 18:29:42 +0530

    App Logo & BootUp Screen

--- aadharApp/navigation/AppNavigator.js
+++ aadharApp/navigation/AppNavigator.js
@@ -6,6 +6,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
 import Login from '../screens/Login';
 import GeneratePIN from '../screens/PINGenerate';
+import GetStarted from '../screens/GetStarted';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Get Started" component={GetStarted} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="PIN Generate" component={GeneratePIN} />
         <Stack.Screen
