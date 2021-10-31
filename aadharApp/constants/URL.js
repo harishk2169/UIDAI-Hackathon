89d2e24283eaa258f24c945041b157827c1ce88f
Harshit Gupta <harshitgupta1023@gmail.com>
2021-10-31 20:29:28 +0530

    Location Configuration

---
+++ aadharApp/constants/URL.js
@@ -0,0 +1,9 @@
+export default {
+  accessTokenAPI:
+    'https://outpost.mapmyindia.com/api/security/oauth/token?grant_type=client_credentials&',
+  textSearchAPI:
+    'https://atlas.mapmyindia.com/api/places/textsearch/json?query=',
+  geoCodeAPI: 'https://atlas.mapmyindia.com/api/places/geocode?address=',
+  distanceMatrix:
+    'https://apis.mapmyindia.com/advancedmaps/v1/f984cbde1825095f34d4b2e443192a18/distance_matrix_eta/driving/',
+};
