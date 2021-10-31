f59057cb297df9d81579a3c8009d62f41680e58a
Harish Sharma <hks212001@gmail.com>
2021-10-31 17:09:33 +0530

    Improved UI

--- aadharApp/navigation/TabNavigator.js
+++ aadharApp/navigation/TabNavigator.js
@@ -7,16 +7,28 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen
-      name="Request"
-      component={Request}
-      options={{
-        tabBarIcon: () => {
-          return <Icon name="map-marked-alt" type="font-awesome" size={24} />;
-        },
-      }}
-    />
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Request') {
+          iconName = 'map-marker';
+        } else if (route.name === 'Received') {
+          iconName = 'users';
+        }
+
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveBackgroundColor: 'tomato',
+      tabBarActiveTintColor: 'white',
+      tabBarInactiveTintColor: 'gray',
+      tabBarLabelStyle: {
+        fontSize: 13,
+      },
+    })}>
+    <Tab.Screen name="Request" component={Request} />
     <Tab.Screen name="Received" component={Received} />
   </Tab.Navigator>
 );
